117e9240bed62311ac7d939d768179c48343114f
pooja <pooja6112002@gmail.com>
2026-04-06 21:28:54 +0530

    first commit

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,9 @@
 const database = require('../database/db');
 const jwt = require("jsonwebtoken");
-const SECRET_KEY = "mysecretkey"; 
+
+const SECRET_KEY = "mysecretkey";
+
+
 const getuserdata = async (req, res) => {
   try {
     const db = await database();
@@ -11,33 +14,37 @@ const getuserdata = async (req, res) => {
       message: 'User records retrieved successfully',
       data: result
     });
-  }
-  catch(err){
+
+  } catch (err) {
     console.error('Error fetching user data:', err);
-    res.status(500).json({ message: 'Internal Server Error' });  
-  } 
-}
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+
 const loginUser = async (req, res) => {
   try {
     const db = await database();
     const collection = db.collection('user');
+
     const user = await collection.findOne({
       username: req.body.username,
       password: req.body.password
     });
 
     if (user) {
-           const token = jwt.sign(
-      {username: user.username },
-      SECRET_KEY,
-      { expiresIn: "1h" }
-    );
+      const token = jwt.sign(
+        { username: user.username },
+        SECRET_KEY,
+        { expiresIn: "1h" }
+      );
 
       res.json({
         success: true,
         message: "Login successful",
-        tokenval:token
+        tokenval: token
       });
+
     } else {
       res.json({
         success: false,
@@ -53,16 +60,28 @@ const loginUser = async (req, res) => {
 
 
 const verifyToken = (req, res, next) => {
-  const token =  req.headers["authorization"];
+  const authHeader = req.headers["authorization"];
 
-  if (!token) {
+  
+  if (!authHeader) {
     return res.status(403).json({
       message: "Token required"
     });
   }
 
+
+  const token = authHeader.split(" ")[0];
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Token missing"
+    });
+  }
+
+  
   jwt.verify(token, SECRET_KEY, (err, decoded) => {
     if (err) {
+      console.log("JWT Error:", err.message); // debug
       return res.status(401).json({
         message: "Invalid token"
       });
@@ -73,7 +92,89 @@ const verifyToken = (req, res, next) => {
     next();
   });
 };
+
 module.exports = {
-  getuserdata,loginUser,verifyToken 
+  getuserdata,
+  loginUser,
+  verifyToken
 };
+
+// const database = require('../database/db');
+// const jwt = require("jsonwebtoken");
+// const SECRET_KEY = "mysecretkey"; 
+// const getuserdata = async (req, res) => {
+//   try {
+//     const db = await database();
+//     const collection = db.collection('user');
+//     const result = await collection.find({}).toArray();
+
+//     res.json({
+//       message: 'User records retrieved successfully',
+//       data: result
+//     });
+//   }
+//   catch(err){
+//     console.error('Error fetching user data:', err);
+//     res.status(500).json({ message: 'Internal Server Error' });  
+//   } 
+// }
+// const loginUser = async (req, res) => {
+//   try {
+//     const db = await database();
+//     const collection = db.collection('user');
+//     const user = await collection.findOne({
+//       username: req.body.username,
+//       password: req.body.password
+//     });
+
+//     if (user) {
+//            const token = jwt.sign(
+//       {username: user.username },
+//       SECRET_KEY,
+//       { expiresIn: "1h" }
+//     );
+
+//       res.json({
+//         success: true,
+//         message: "Login successful",
+//         tokenval:token
+//       });
+//     } else {
+//       res.json({
+//         success: false,
+//         message: "Invalid username or password"
+//       });
+//     }
+
+//   } catch (err) {
+//     console.error('Login error:', err);
+//     res.status(500).json({ message: 'Internal Server Error' });
+//   }
+// };
+
+
+// const verifyToken = (req, res, next) => {
+//   const token =  req.headers["authorization"];
+
+//   if (!token) {
+//     return res.status(403).json({
+//       message: "Token required"
+//     });
+//   }
+
+//   jwt.verify(token, SECRET_KEY, (err, decoded) => {
+//     if (err) {
+//       return res.status(401).json({
+//         message: "Invalid token"
+//       });
+//     }
+
+//     req.user = decoded;
+//     console.log("Decoded token:", decoded);
+//     next();
+//   });
+// };
+// module.exports = {
+//   getuserdata,loginUser,verifyToken 
+// };
 
